2638b79baf2858ed4a1c9a35d8362e8f925ecd98
ArmandoHdz <1530343@upv.edu.mx>
2018-05-31 09:53:59 -0500

    practica12-fase1

---
+++ practica12/views/modules/navegacion.php
@@ -0,0 +1,10 @@
+<nav>
+	<ul>
+		<!-- Pra navegar al URL podemos hacerlo mediante la variable GET, la cual la toma del URL, se representa por el simbolo "?"-->
+		<li><a href="index.php?action=ingresar">Ingreso</a></li>
+		<li><a href="index.php?action=inventario">Inventarios</a></li>
+		<li><a href="index.php?action=categorias">Categorias</a></li>
+		<li><a href="index.php?action=usuarios">Usuarios</a></li>
+		<li><a href="index.php?action=salir">Salir</a></li>
+	</ul>
+</nav>
